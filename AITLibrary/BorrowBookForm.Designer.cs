e80c127bfd246c3ec92ef5cb390757943d5fbe88
kennedias <5399@ait-sydney.net.au>
2016-09-21 10:54:50 +1000

    Bopok borrow

--- AITLibrary/BorrowBookForm.Designer.cs
+++ AITLibrary/BorrowBookForm.Designer.cs
@@ -36,13 +36,24 @@
             this.textBoxBookName = new System.Windows.Forms.TextBox();
             this.labelBookName = new System.Windows.Forms.Label();
             this.labelSearchBy = new System.Windows.Forms.Label();
-            this.labelOr = new System.Windows.Forms.Label();
+            this.labelOrBookSearch = new System.Windows.Forms.Label();
             this.textBoxISBN = new System.Windows.Forms.TextBox();
             this.labelISBN = new System.Windows.Forms.Label();
             this.dataGridViewListBooks = new System.Windows.Forms.DataGridView();
             this.buttonBorrowBook = new System.Windows.Forms.Button();
+            this.groupBoxStudentSearch = new System.Windows.Forms.GroupBox();
+            this.labelSearchStudentby = new System.Windows.Forms.Label();
+            this.labelStudentName = new System.Windows.Forms.Label();
+            this.labelOrStudentSearch = new System.Windows.Forms.Label();
+            this.textBoxStudentName = new System.Windows.Forms.TextBox();
+            this.labelStudentId = new System.Windows.Forms.Label();
+            this.textBoxStudentId = new System.Windows.Forms.TextBox();
+            this.buttonSearchStudent = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.groupBoxSearchBooks.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewListBooks)).BeginInit();
+            this.groupBoxStudentSearch.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // groupBoxSearchBooks
@@ -54,19 +65,19 @@
             this.groupBoxSearchBooks.Controls.Add(this.textBoxBookName);
             this.groupBoxSearchBooks.Controls.Add(this.labelBookName);
             this.groupBoxSearchBooks.Controls.Add(this.labelSearchBy);
-            this.groupBoxSearchBooks.Controls.Add(this.labelOr);
+            this.groupBoxSearchBooks.Controls.Add(this.labelOrBookSearch);
             this.groupBoxSearchBooks.Controls.Add(this.textBoxISBN);
             this.groupBoxSearchBooks.Controls.Add(this.labelISBN);
-            this.groupBoxSearchBooks.Location = new System.Drawing.Point(19, 143);
+            this.groupBoxSearchBooks.Location = new System.Drawing.Point(19, 213);
             this.groupBoxSearchBooks.Name = "groupBoxSearchBooks";
-            this.groupBoxSearchBooks.Size = new System.Drawing.Size(207, 268);
+            this.groupBoxSearchBooks.Size = new System.Drawing.Size(207, 239);
             this.groupBoxSearchBooks.TabIndex = 5;
             this.groupBoxSearchBooks.TabStop = false;
             this.groupBoxSearchBooks.Text = "Books Search ";
             // 
             // buttonSearch
             // 
-            this.buttonSearch.Location = new System.Drawing.Point(97, 197);
+            this.buttonSearch.Location = new System.Drawing.Point(97, 169);
             this.buttonSearch.Name = "buttonSearch";
             this.buttonSearch.Size = new System.Drawing.Size(100, 23);
             this.buttonSearch.TabIndex = 10;
@@ -76,7 +87,7 @@
             // 
             // buttonListAllBooks
             // 
-            this.buttonListAllBooks.Location = new System.Drawing.Point(97, 234);
+            this.buttonListAllBooks.Location = new System.Drawing.Point(97, 198);
             this.buttonListAllBooks.Name = "buttonListAllBooks";
             this.buttonListAllBooks.Size = new System.Drawing.Size(100, 23);
             this.buttonListAllBooks.TabIndex = 1;
@@ -121,18 +132,18 @@
             this.labelSearchBy.AutoSize = true;
             this.labelSearchBy.Location = new System.Drawing.Point(7, 20);
             this.labelSearchBy.Name = "labelSearchBy";
-            this.labelSearchBy.Size = new System.Drawing.Size(58, 13);
+            this.labelSearchBy.Size = new System.Drawing.Size(86, 13);
             this.labelSearchBy.TabIndex = 3;
-            this.labelSearchBy.Text = "Search by:";
+            this.labelSearchBy.Text = "Search Book by:";
             // 
-            // labelOr
+            // labelOrBookSearch
             // 
-            this.labelOr.AutoSize = true;
-            this.labelOr.Location = new System.Drawing.Point(54, 75);
-            this.labelOr.Name = "labelOr";
-            this.labelOr.Size = new System.Drawing.Size(16, 13);
-            this.labelOr.TabIndex = 2;
-            this.labelOr.Text = "or";
+            this.labelOrBookSearch.AutoSize = true;
+            this.labelOrBookSearch.Location = new System.Drawing.Point(54, 75);
+            this.labelOrBookSearch.Name = "labelOrBookSearch";
+            this.labelOrBookSearch.Size = new System.Drawing.Size(16, 13);
+            this.labelOrBookSearch.TabIndex = 2;
+            this.labelOrBookSearch.Text = "or";
             // 
             // textBoxISBN
             // 
@@ -156,15 +167,15 @@
             this.dataGridViewListBooks.AllowUserToDeleteRows = false;
             this.dataGridViewListBooks.AllowUserToOrderColumns = true;
             this.dataGridViewListBooks.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridViewListBooks.Location = new System.Drawing.Point(236, 149);
+            this.dataGridViewListBooks.Location = new System.Drawing.Point(236, 219);
             this.dataGridViewListBooks.Name = "dataGridViewListBooks";
             this.dataGridViewListBooks.ReadOnly = true;
-            this.dataGridViewListBooks.Size = new System.Drawing.Size(817, 262);
+            this.dataGridViewListBooks.Size = new System.Drawing.Size(817, 233);
             this.dataGridViewListBooks.TabIndex = 4;
             // 
             // buttonBorrowBook
             // 
-            this.buttonBorrowBook.Location = new System.Drawing.Point(118, 414);
+            this.buttonBorrowBook.Location = new System.Drawing.Point(126, 477);
             this.buttonBorrowBook.Name = "buttonBorrowBook";
             this.buttonBorrowBook.Size = new System.Drawing.Size(100, 23);
             this.buttonBorrowBook.TabIndex = 6;
@@ -172,24 +183,111 @@
             this.buttonBorrowBook.UseVisualStyleBackColor = true;
             this.buttonBorrowBook.Click += new System.EventHandler(this.buttonBorrowBook_Click);
             // 
+            // groupBoxStudentSearch
+            // 
+            this.groupBoxStudentSearch.Controls.Add(this.buttonSearchStudent);
+            this.groupBoxStudentSearch.Controls.Add(this.textBoxStudentId);
+            this.groupBoxStudentSearch.Controls.Add(this.labelStudentId);
+            this.groupBoxStudentSearch.Controls.Add(this.textBoxStudentName);
+            this.groupBoxStudentSearch.Controls.Add(this.labelOrStudentSearch);
+            this.groupBoxStudentSearch.Controls.Add(this.labelStudentName);
+            this.groupBoxStudentSearch.Controls.Add(this.labelSearchStudentby);
+            this.groupBoxStudentSearch.Location = new System.Drawing.Point(19, 46);
+            this.groupBoxStudentSearch.Name = "groupBoxStudentSearch";
+            this.groupBoxStudentSearch.Size = new System.Drawing.Size(207, 161);
+            this.groupBoxStudentSearch.TabIndex = 7;
+            this.groupBoxStudentSearch.TabStop = false;
+            this.groupBoxStudentSearch.Text = "Student Search";
+            // 
+            // labelSearchStudentby
+            // 
+            this.labelSearchStudentby.AutoSize = true;
+            this.labelSearchStudentby.Location = new System.Drawing.Point(6, 16);
+            this.labelSearchStudentby.Name = "labelSearchStudentby";
+            this.labelSearchStudentby.Size = new System.Drawing.Size(98, 13);
+            this.labelSearchStudentby.TabIndex = 8;
+            this.labelSearchStudentby.Text = "Search Student by:";
+            // 
+            // labelStudentName
+            // 
+            this.labelStudentName.AutoSize = true;
+            this.labelStudentName.Location = new System.Drawing.Point(24, 83);
+            this.labelStudentName.Name = "labelStudentName";
+            this.labelStudentName.Size = new System.Drawing.Size(35, 13);
+            this.labelStudentName.TabIndex = 9;
+            this.labelStudentName.Text = "Name";
+            // 
+            // labelOrStudentSearch
+            // 
+            this.labelOrStudentSearch.AutoSize = true;
+            this.labelOrStudentSearch.Location = new System.Drawing.Point(55, 60);
+            this.labelOrStudentSearch.Name = "labelOrStudentSearch";
+            this.labelOrStudentSearch.Size = new System.Drawing.Size(16, 13);
+            this.labelOrStudentSearch.TabIndex = 11;
+            this.labelOrStudentSearch.Text = "or";
+            // 
+            // textBoxStudentName
+            // 
+            this.textBoxStudentName.Location = new System.Drawing.Point(61, 80);
+            this.textBoxStudentName.Name = "textBoxStudentName";
+            this.textBoxStudentName.Size = new System.Drawing.Size(136, 20);
+            this.textBoxStudentName.TabIndex = 12;
+            // 
+            // labelStudentId
+            // 
+            this.labelStudentId.AutoSize = true;
+            this.labelStudentId.Location = new System.Drawing.Point(41, 39);
+            this.labelStudentId.Name = "labelStudentId";
+            this.labelStudentId.Size = new System.Drawing.Size(18, 13);
+            this.labelStudentId.TabIndex = 13;
+            this.labelStudentId.Text = "ID";
+            // 
+            // textBoxStudentId
+            // 
+            this.textBoxStudentId.Location = new System.Drawing.Point(61, 36);
+            this.textBoxStudentId.Name = "textBoxStudentId";
+            this.textBoxStudentId.Size = new System.Drawing.Size(136, 20);
+            this.textBoxStudentId.TabIndex = 8;
+            // 
+            // buttonSearchStudent
+            // 
+            this.buttonSearchStudent.Location = new System.Drawing.Point(122, 122);
+            this.buttonSearchStudent.Name = "buttonSearchStudent";
+            this.buttonSearchStudent.Size = new System.Drawing.Size(75, 23);
+            this.buttonSearchStudent.TabIndex = 14;
+            this.buttonSearchStudent.Text = "Search";
+            this.buttonSearchStudent.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(236, 46);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(817, 161);
+            this.dataGridView1.TabIndex = 8;
+            // 
             // BorrowBookForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1076, 551);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.groupBoxStudentSearch);
             this.Controls.Add(this.buttonBorrowBook);
             this.Controls.Add(this.groupBoxSearchBooks);
             this.Controls.Add(this.dataGridViewListBooks);
             this.Name = "BorrowBookForm";
             this.Text = "BorrowBookForm";
-            this.Controls.SetChildIndex(this.dataGridViewListBooks, 0);
-            this.Controls.SetChildIndex(this.groupBoxSearchBooks, 0);
-            this.Controls.SetChildIndex(this.buttonBorrowBook, 0);
             this.groupBoxSearchBooks.ResumeLayout(false);
             this.groupBoxSearchBooks.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewListBooks)).EndInit();
+            this.groupBoxStudentSearch.ResumeLayout(false);
+            this.groupBoxStudentSearch.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -203,10 +301,19 @@
         private System.Windows.Forms.TextBox textBoxBookName;
         private System.Windows.Forms.Label labelBookName;
         private System.Windows.Forms.Label labelSearchBy;
-        private System.Windows.Forms.Label labelOr;
+        private System.Windows.Forms.Label labelOrBookSearch;
         private System.Windows.Forms.TextBox textBoxISBN;
         private System.Windows.Forms.Label labelISBN;
         private System.Windows.Forms.DataGridView dataGridViewListBooks;
         private System.Windows.Forms.Button buttonBorrowBook;
+        private System.Windows.Forms.GroupBox groupBoxStudentSearch;
+        private System.Windows.Forms.Label labelOrStudentSearch;
+        private System.Windows.Forms.Label labelStudentName;
+        private System.Windows.Forms.Label labelSearchStudentby;
+        private System.Windows.Forms.Button buttonSearchStudent;
+        private System.Windows.Forms.TextBox textBoxStudentId;
+        private System.Windows.Forms.Label labelStudentId;
+        private System.Windows.Forms.TextBox textBoxStudentName;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
--- AITLibrary/LybraryBaseForm.Designer.cs
+++ AITLibrary/LybraryBaseForm.Designer.cs
@@ -155,14 +155,14 @@
             this.administrationToolStripMenuItem});
             this.menuStripSystemLybrary.Location = new System.Drawing.Point(0, 0);
             this.menuStripSystemLybrary.Name = "menuStripSystemLybrary";
-            this.menuStripSystemLybrary.Size = new System.Drawing.Size(1076, 24);
+            this.menuStripSystemLybrary.Size = new System.Drawing.Size(1108, 24);
             this.menuStripSystemLybrary.TabIndex = 0;
             // 
             // groupBoxSystemMessage
             // 
             this.groupBoxSystemMessage.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.groupBoxSystemMessage.Controls.Add(this.labelSystemMessage);
-            this.groupBoxSystemMessage.Location = new System.Drawing.Point(12, 540);
+            this.groupBoxSystemMessage.Location = new System.Drawing.Point(12, 597);
             this.groupBoxSystemMessage.Name = "groupBoxSystemMessage";
             this.groupBoxSystemMessage.Size = new System.Drawing.Size(1008, 32);
             this.groupBoxSystemMessage.TabIndex = 1;
@@ -182,7 +182,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1076, 584);
+            this.ClientSize = new System.Drawing.Size(1108, 634);
             this.Controls.Add(this.groupBoxSystemMessage);
             this.Controls.Add(this.menuStripSystemLybrary);
             this.MainMenuStrip = this.menuStripSystemLybrary;
